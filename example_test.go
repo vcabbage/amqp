ad0085cc1af5991dd422ebf18e86d7744505adff
Kale Blankenship <kale@lemnisys.com>
2018-01-11 19:37:09 -0800

    Add sending example

--- example_test.go
+++ example_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"pack.ag/amqp"
 )
@@ -24,28 +25,57 @@ func Example() {
 		log.Fatal("Creating AMQP session:", err)
 	}
 
-	// Create a receiver
-	receiver, err := session.NewReceiver(
-		amqp.LinkAddress("/queue-name"),
-		amqp.LinkCredit(10),
-	)
-	if err != nil {
-		log.Fatal("Creating receiver link:", err)
-	}
+	ctx := context.Background()
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	for {
-		// Receive next message
-		msg, err := receiver.Receive(ctx)
+	// Send a message
+	{
+		// Create a sender
+		sender, err := session.NewSender(
+			amqp.LinkAddress("/queue-name"),
+		)
 		if err != nil {
-			log.Fatal("Reading message from AMQP:", err)
+			log.Fatal("Creating sender link:", err)
 		}
 
-		// Accept message
-		msg.Accept()
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 
-		fmt.Printf("Message received: %s\n", msg.Data)
+		// Send message
+		err = sender.Send(ctx, &amqp.Message{
+			Data: []byte("Hello!"),
+		})
+		if err != nil {
+			log.Fatal("Sending message:", err)
+		}
+
+		cancel()
+		sender.Close()
+	}
+
+	// Continuously read messages
+	{
+		// Create a receiver
+		receiver, err := session.NewReceiver(
+			amqp.LinkAddress("/queue-name"),
+			amqp.LinkCredit(10),
+		)
+		if err != nil {
+			log.Fatal("Creating receiver link:", err)
+		}
+
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		for {
+			// Receive next message
+			msg, err := receiver.Receive(ctx)
+			if err != nil {
+				log.Fatal("Reading message from AMQP:", err)
+			}
+
+			// Accept message
+			msg.Accept()
+
+			fmt.Printf("Message received: %s\n", msg.Data)
+		}
 	}
 }
